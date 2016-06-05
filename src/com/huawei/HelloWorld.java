474fd44ebbae3ef651fa8027dbc694c4b74d1886
sylow619 <576628255@qq.com>
2016-06-05 23:30:25 +0800

    测试

---
+++ src/com/huawei/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.huawei;
+
+/**
+ * Created by aipaopao on 16-6-5.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");;
+    }
+}
